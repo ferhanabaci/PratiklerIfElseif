920624e3391b9100e86c22f3ff529627a2f0d283
Ferhan Abaci <ferhanabacii@gmail.com>
2022-02-24 00:38:46 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time>=6 && time <11){
+                
+                 Console.WriteLine("Günaydın");
+            }
+            else if(time<=18){
+                
+                Console.WriteLine("İyi günler");
+
+            }
+               
+            else{
+                  Console.WriteLine("iyi geceler");
+            
+            }
+            string sonuc = time <=18 ?"iyi günler":"İyi geceler";
+            sonuc = time >=6 && time<11 ?"Günaydın "  : time <= 18 ? "iyi günler":"iyi geceler";
+
+            Console.WriteLine(sonuc);
+               
+        }
+}
+
+}
